579abb92c3139b6733727ec0e87ff3342f158240
Rohan Kumar <rohandbg2019.rk@gmail.com>
2022-11-27 16:34:21 +0530

    sample

--- src/App.js
+++ src/App.js
@@ -1,10 +1,87 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
+import {useState} from "react";
 function App() {
+  const [cards]=useState([
+    {
+      title: 'All India Senior Ranking Badmintion',
+      text:(
+        <p>
+          Tournament
+          <br/>Final<br/>v/s<br/>
+          <p><img src='./f.png'/> 9-21,21-10,21-15   <img src='./f.png'/> </p>
+          <img src='./logo_white.png'/>
+        </p>
+        
+      ),
+    },
+    {
+      title: 'All India Senior Ranking Badmintion',
+      text:(
+        <p>
+          Tournament
+          <br/>Final<br/>v/s<br/>
+          <p><img src='./f.png'/> 9-21,21-10,21-15   <img src='./f.png'/> </p>
+          <img src='./logo_white.png'/>
+        </p>
+        
+      ),
+      
+    },
+    {
+      title: 'All India Senior Ranking Badmintion',
+      text:(
+        <p>
+          Tournament
+          <br/>Final<br/>v/s<br/>
+          <p><img src='./f.png'/> 9-21,21-10,21-15   <img src='./f.png'/> </p>
+          <img src='./logo_white.png'/>
+        </p>
+        
+      ),
+    },
+    {
+      title: 'All India Senior Ranking Badmintion',
+      text:(
+        <p>
+          Tournament
+          <br/>Final<br/>v/s<br/>
+          
+           <p><img src='./f.png'/> 9-21,21-10,21-15   <img src='./f.png'/> </p>
+          
+           <img src='./logo_white.png'/>
+        </p>
+        
+      ),
+    },
+    {
+      title: 'All India Senior Ranking Badmintion',
+      text:(
+        <p>
+          Tournament
+          <br/>Final<br/>v/s<br/>
+          <p><img src='./f.png'/> 9-21,21-10,21-15   <img src='./f.png'/> </p>
+          <img src='./logo_white.png'/>
+        </p>
+      ),
+    },
+    {
+      title: 'All India Senior Ranking Badmintion',
+      text:(
+        <p>
+          Tournament
+          <br/>Final<br/>v/s<br/>
+          <p><img src='./f.png'/> 9-21,21-10,21-15   <img src='./f.png'/> </p>
+          <img src='./logo_white.png'/>
+        </p>
+        
+      ),
+    },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +94,27 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <div className="header">
+        <h1>International Matches</h1>
+        <input type="text" className="search"placeholders="Search for Matches" ></input>
+        <section>
+          <div className="container">
+            {/* <h1>Responsive Cards</h1> */}
+            <div className="cards">
+            {
+              cards.map((card,i)=>(
+                <div key={i} className="card">
+                <h3>{card.title}</h3>
+                <p>{card.text}</p>
+              </div>
+              ))
+            }
+              
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
